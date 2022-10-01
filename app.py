e5f92177ffb679a3cc26c1f5b13457ecac790d49
Vamsi Atluri <vamc19@gmail.com>
2022-09-30 22:52:44 -0700

    Add routes to persist kev value pairs in redis

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import logging
+import os
 import socket
 import sqlite3
 import time
 from uuid import uuid4
-import os
 
+import redis
 from flask import Flask, g, request, redirect, url_for
 
 __version__ = 0.1
@@ -14,6 +15,9 @@ logging.basicConfig(level=logging.INFO)
 # Path to sqlite database file is passed as an environment variable
 sqlite_path = os.getenv('SQLITE_PATH', ':memory:')
 sqlite = sqlite3.connect(sqlite_path, check_same_thread=False)
+
+# Redis hostname and port are passed as environment variables
+r = redis.Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=os.getenv('REDIS_PORT', 6379))
 
 
 @app.before_first_request
@@ -58,7 +62,7 @@ def sqlite_insert():
         sqlite.execute('INSERT INTO dume VALUES (?, ?, ?)', (request.json['key'], request.json['value'], g.request_id))
         sqlite.commit()
     except sqlite3.IntegrityError:
-        return "key exists", 400
+        return 'key exists', 400
     return sqlite_select(request.json['key'])
 
 
@@ -67,8 +71,22 @@ def sqlite_select(key):
     cur = sqlite.execute('SELECT key, value, reqId FROM dume WHERE key = ?', (key,))
     result = cur.fetchall()
     if not result:
-        return {}, 404
+        return 'not found', 404
     return {'key': result[0][0], 'value': result[0][1], 'reqId': result[0][2]}
+
+
+@app.route('/redis', methods=['PUT'])
+def redis_insert():
+    r.set(request.json['key'], request.json['value'])
+    return redis_get(request.json['key'])
+
+
+@app.route('/redis/<key>', methods=['GET'])
+def redis_get(key):
+    value = r.get(key)
+    if not value:
+        return 'not found', 404
+    return {'key': key, 'value': value.decode('utf-8')}
 
 
 @app.route('/health', methods=['GET'])
